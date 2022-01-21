3c200fbcfe0545b18b07fd27567eb43c2a7427b2
Ryan Burrow <RyanDBurrow@vt.edu>
2022-01-21 14:07:07 -0500

    Day 2 part 1 done

---
+++ day2/README.md
@@ -0,0 +1,11 @@
+Rust implementation of [Day 2](https://adventofcode.com/2021/day/2) of Advent of code
+
+Build and run with 
+```
+# Part 1
+rustc navigate-pt1.rs
+./depth < input.txt
+# Part 2
+rustc navigate-pt2.rs
+./depth < input.txt
+```
---
+++ day2/navigate-pt1.rs
@@ -0,0 +1,29 @@
+#![crate_name = "navigate"]
+use std::io::prelude::*;
+
+// Depth increase counter
+// This is my Rust implementation of Day 1 of the Advent of code
+// This program takes a list of depths from stdin
+// and outputs the number of times that value increases
+
+fn main() -> std::io::Result <()>{
+    let mut pos: i32 = 0;
+    let mut depth: i32 = 0;
+    let stdin = std::io::stdin();
+    let handle = stdin.lock();
+
+    for line in handle.lines() {
+        let data = line?;
+        let v: Vec<&str> = data.split_whitespace().collect();
+        match v[0] {
+            "forward" => pos += v[1].parse::<i32>().unwrap(),
+            "down" => depth += v[1].parse::<i32>().unwrap(),
+            "up" => depth -= v[1].parse::<i32>().unwrap(),
+            _ => println!("Unrecognized command"),
+        };
+        // println!("{:?}: {}, {}", v, v[0], v[1]);
+    }
+    println!("Depth: {}, Position: {}, Mult: {}", depth, pos, depth * pos);
+
+    Ok(())
+}
