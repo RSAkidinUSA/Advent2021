86b494e15d075815dcc05e2796675c2ba141b3e0
Ryan Burrow <RyanDBurrow@vt.edu>
2022-02-01 16:27:23 -0500

    Day 3 part 1 done

---
+++ day3/README.md
@@ -0,0 +1,11 @@
+Rust implementation of [Day 3](https://adventofcode.com/2021/day/3) of Advent of code
+
+Build and run with 
+```
+# Part 1
+rustc diagnostic-pt1.rs
+./diagnostic < input.txt
+# Part 2
+rustc diagnostic-pt2.rs
+./diagnostic < input.txt
+```
---
+++ day3/diagnostic-pt1.rs
@@ -0,0 +1,48 @@
+#![crate_name = "diagnostic"]
+use std::io::prelude::*;
+
+// Depth increase counter
+// This is my Rust implementation of Day 1 of the Advent of code
+// This program takes a list of depths from stdin
+// and outputs the number of times that value increases
+
+fn main() -> std::io::Result <()>{
+    let stdin = std::io::stdin();
+    // Figure out how long each binary string is
+    let len = stdin.read_line(&mut String::new())? - 1;
+    println!("Binary string length: {}", len);
+    // NOTE: bit_count is in reverse bit order for parsing reasons - e.g. MSB = bit_count[0]
+    let mut bit_count: Vec<i32> = vec![0; len];
+    let handle = stdin.lock();
+
+    // Read in the data
+    for line in handle.lines() {
+        let data = line?;
+        let val = data.as_bytes();
+        for i in 0..=len - 1 {
+            bit_count[i] = match val[i] as char {
+                '1' => bit_count[i] + 1,
+                '0' => bit_count[i] - 1,
+                _ => {println!("Unrecognized Value");
+                      0},
+            }
+        };
+        // println!("{:?}: {}, {}", v, v[0], v[1]);
+    }
+    // Data processing
+    let mut gamma: i32 = 0;
+    for i in 0..=len - 1 {
+        gamma = gamma << 1;
+        if bit_count[i] > 0 {
+            gamma |= 1;
+        } else if bit_count[i] == 0 {
+            println!("Error, equal number of 1's and 0's for bit {}", i);
+        }
+    }
+    let epsilon = gamma ^ ((1 << len) - 1);
+
+    println!("Gamma: {:0len$b}, Epsilon: {:0len$b}, Consumption: {}",
+            gamma, epsilon, gamma * epsilon, len = len);
+
+    Ok(())
+}
